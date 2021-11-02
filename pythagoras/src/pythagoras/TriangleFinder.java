e0c30de08f8ba8602cd01d6bc24a69f3b518ffd9
olive <olive@DESKTOP-ON16020>
2021-11-02 15:13:17 -0700

    changed TriangleFinder constructor

--- pythagoras/src/pythagoras/TriangleFinder.java
+++ pythagoras/src/pythagoras/TriangleFinder.java
@@ -4,10 +4,11 @@ public class TriangleFinder {
 	private double side1;
 	private double side2;
 	
-	public TriangleFinder(double sideLength1, double sideLength2) {
-		side1 = sideLength1;
-		side2 = sideLength2;
-		
+	
+	public TriangleFinder(double side1, double side2) {
+		super();
+		this.side1 = side1;
+		this.side2 = side2;
 	}
 	public double findHypotenuse() {
 		double hypotenuse = Math.sqrt((Math.pow(side1, 2) + Math.pow(side2, 2)));
