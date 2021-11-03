65b2f6f3d3cc9adacb825cdb58b84fa51c0416a4
olive <olive@DESKTOP-ON16020>
2021-11-02 20:55:30 -0700

    DistanceTester and Distance functionality

---
+++ Distance conversions/src/Distance.java
@@ -0,0 +1,21 @@
+
+public class Distance {
+	private double kilometers;
+
+	public Distance(double kilometers) {
+		this.kilometers = kilometers;
+	}
+	public double toMiles() {
+		return kilometers / 1.609;
+	}
+	public double toLeagues() {
+		return kilometers / 5.556;
+	}
+	public double doubleMiles() {
+		return 2 * (kilometers / 1.609);
+	}
+	public double getKilometers() {
+		return kilometers;
+	}
+	
+}
