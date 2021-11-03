73faad0964020521a22d6da495ecd3693a1f6627
oliverstivers <oliverstivers@gmail.com>
2021-11-03 13:57:49 -0700

    added combinationLock functionality

---
+++ Combination Lock/src/CombinationLock.java
@@ -0,0 +1,22 @@
+
+public class CombinationLock {
+	private String combo;
+	public CombinationLock(String combo) {
+		this.combo = combo;
+	}
+	public String getClue(String clue) {
+		String newString = "";
+		for(int i = 0; i < clue.length(); i++) {
+			if(clue.substring(i, i+1).equals(combo.substring(i, i+1))) {
+				newString += clue.substring(i, i+1);
+			}
+			else if(combo.contains(clue.substring(i, i+1))) {
+				newString += "+";
+			}
+			else {
+				newString += "*";
+			}
+		}
+		return  newString;
+	}
+}
---
+++ Combination Lock/src/CombinationLockTester.java
@@ -0,0 +1,10 @@
+
+public class CombinationLockTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CombinationLock combo = new CombinationLock("hell");
+		System.out.println(combo.getClue("hell"));
+	}
+
+}
