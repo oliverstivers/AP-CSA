0866b4c79634aa6a7c753a1b6028c18d9ceceb88
oliverstivers <oliverstivers@gmail.com>
2021-11-04 16:00:32 -0700

    removed comments

--- Combination Lock/src/CombinationLockTester.java
+++ Combination Lock/src/CombinationLockTester.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
-//comment
-//hello
-//comment added!
-public class CombinationLockTester {
+
+public class CombinationLockTester 
+{
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		CombinationLock combo = new CombinationLock("hell");
 		System.out.println("Enter your guess: ");
         String guess = in.nextLine();
-        while(guess.length() != 4){
+        while(guess.length() != 4)
+        {
             System.out.println("Guess needs to be 4 letters, please try again: ");
             guess = in.nextLine();
         }
         System.out.println(combo.getClue(guess));
-			while(combo.getClue(guess).contains("*") || combo.getClue(guess).contains("+")) {
+			while(combo.getClue(guess).contains("*") || combo.getClue(guess).contains("+")) 
+            {
 			System.out.println("Incorrect, try again: ");
 			guess = in.nextLine();
-            while(guess.length() != 4){
+            while(guess.length() != 4)
+            {
                 System.out.println("Guess needs to be 4 letters, please try again: ");
                 guess = in.nextLine();
             }
             System.out.println(combo.getClue(guess));
-			if(!(combo.getClue(guess).contains("*") || combo.getClue(guess).contains("+"))){
+			if(!(combo.getClue(guess).contains("*") || combo.getClue(guess).contains("+")))
+            {
 				break;
 			}
             
