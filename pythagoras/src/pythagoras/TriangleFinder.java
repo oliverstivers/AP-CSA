de4684859c83ebf41d1a0f6cd50914777b91a0c1
oliverstivers <oliverstivers@gmail.com>
2021-11-04 15:45:42 -0700

    added static methods

--- pythagoras/src/pythagoras/TriangleFinder.java
+++ pythagoras/src/pythagoras/TriangleFinder.java
@@ -1,20 +1,15 @@
 package pythagoras;
 
 public class TriangleFinder {
-	private double side1;
-	private double side2;
 	
 	
-	public TriangleFinder(double side1, double side2) {
-		super();
-		this.side1 = side1;
-		this.side2 = side2;
-	}
-	public double findHypotenuse() {
+	
+	
+	public static double findHypotenuse(double side1, double side2) {
 		double hypotenuse = Math.sqrt((Math.pow(side1, 2) + Math.pow(side2, 2)));
 		return hypotenuse;
 	}
-	public double findLeg() {
+	public static double findLeg(double side1, double side2) {
 		double leg = Math.sqrt((Math.pow(side2, 2) - Math.pow(side1, 2)));
 		return leg;
 	}
