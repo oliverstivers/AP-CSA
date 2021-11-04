7aaf15a397577c936724ec5db8604f13c17e0aab
oliverstivers <oliverstivers@gmail.com>
2021-11-04 11:26:54 -0700

    Update CombinationLockTester.java

--- Combination Lock/src/CombinationLockTester.java
+++ Combination Lock/src/CombinationLockTester.java
@@ -1,10 +1,10 @@
-
+import java.util.Scanner;
 public class CombinationLockTester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CombinationLock combo = new CombinationLock("hell");
-		System.out.println(combo.getClue("hell"));
+		
 	}
 
 }
