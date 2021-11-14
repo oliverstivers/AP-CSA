707969803d46cec4c2aed09ed8fbe402a1dad668
oliverstivers <oliverstivers@gmail.com>
2021-11-13 20:33:01 -0800

    takes ArrayList as parameter

--- Combination Lock/src/WordGiver.java
+++ Combination Lock/src/WordGiver.java
@@ -1,5 +1,6 @@
+import java.util.*;
 public class WordGiver {
-    public static int giveRandom(String[] wordList) {
-        return (int)(Math.random() * wordList.length);
+    public static int giveRandom(ArrayList<String> list) {
+        return (int)(Math.random() * list.size());
     }
 }
