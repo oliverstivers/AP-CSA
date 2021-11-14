8e5266583533455690adddb9ab7772363be7c201
oliverstivers <oliverstivers@gmail.com>
2021-11-13 20:31:02 -0800

    Revert "takes ArrayList as parameter"
    This reverts commit c8eb4c770cbf81aa317fee645b6f0cd4702ae34b.

--- Combination Lock/src/CombinationLockTester.java
+++ Combination Lock/src/CombinationLockTester.java
@@ -7,30 +7,31 @@ public class CombinationLockTester
 {
 
 	public static void main(String[] args) throws FileNotFoundException{
-		//creates new ArrayList from commonwords.txt
         ArrayList<String> words = new ArrayList<String>();
         File f = new File("commonwords.txt");
         Scanner scF = new Scanner(f);
         while(scF.hasNext()){
             String line = scF.nextLine();
             Scanner scR = new Scanner(line);
-            while(scR.hasNext())
-            {
+            while(scR.hasNext()){
                 String word1 = scR.next();
                 words.add(word1);
             }
             scR.close();
         }
-        
+        String[] wordList = new String[words.size()];
+        for(int i = 0; i < words.size(); i++){
+            wordList[i] = words.get(i);
+        }
         
 		Scanner in = new Scanner(System.in);
         int guessCounter = 0;
-        //gives random number index 
-		CombinationLock combo = new CombinationLock(words.get(WordGiver.giveRandom(words)));
+        
+		CombinationLock combo = new CombinationLock(wordList[WordGiver.giveRandom(wordList)]);
+        
 		System.out.println("Enter your guess, or type 'quit' to cancel: ");
         String guess = in.nextLine();
         guessCounter ++;
-        //makes sure guess == 4 characters
         while(guess.length() != 4 && !guess.equals("quit"))
         {
             System.out.println("Guess must be 4 letters, please try again: ");
@@ -42,23 +43,26 @@ public class CombinationLockTester
         
 			while(combo.getClue(guess).contains("*") || combo.getClue(guess).contains("+") && !guess.equals("quit"))
             {
-				System.out.println("Guess is incorrect, please try again (quit to end): ");
-				guess = in.nextLine();
-	            guessCounter++;
-	            while(guess.length() != 4 && !guess.equals("quit"))
-	            {
-	                System.out.println("Guess must be 4 letters, please try again: ");
-	                guess = in.nextLine();
-	            }
-	            if(!guess.equals("quit"))
-	            {
-	            System.out.println(combo.getClue(guess));
-	            }
-				else if(!(combo.getClue(guess).contains("*") || combo.getClue(guess).contains("+")) || guess.equals("quit"))
-	            {
-					guessCounter++;
-	                break;
-	            }		
+			System.out.println("Guess is incorrect, please try again (quit to end): ");
+			guess = in.nextLine();
+            guessCounter++;
+            while(guess.length() != 4 && !guess.equals("quit"))
+            {
+                System.out.println("Guess must be 4 letters, please try again: ");
+                guess = in.nextLine();
+                
+            }
+            if(!guess.equals("quit")){
+            System.out.println(combo.getClue(guess));
+            }
+			else if(!(combo.getClue(guess).contains("*") || combo.getClue(guess).contains("+")) || guess.equals("quit"))
+            {
+				guessCounter++;
+                break;
+                
+			}
+            
+			
 		}
     }
 		if(guess.equals("quit")){
@@ -68,6 +72,11 @@ public class CombinationLockTester
             System.out.println("Good job! It took you " + guessCounter + " guesses.");
         }
         in.close();
-        scF.close();      
+        scF.close();
+        
+        
 	}
+    
+
+
 }
