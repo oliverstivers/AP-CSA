944df9eb00cab1ec1681f5c936c1d8dcfd044e41
oliverstivers <oliverstivers@gmail.com>
2021-11-15 14:48:17 -0800

    Update CombinationLock.java

--- Combination Lock/src/CombinationLock.java
+++ Combination Lock/src/CombinationLock.java
@@ -1,13 +1,18 @@
 
 public class CombinationLock {
 	private String combo;
+    
 	public CombinationLock(String combo) {
 		this.combo = combo;
 	}
 	public String getClue(String clue) {
 		String newString = "";
+        String[] letterList = new String[clue.length()];
+        for(int i = 0; i < clue.length(); i++){
+            letterList[i] = clue.substring(i, i+1);
+        }
 		for(int i = 0; i < clue.length(); i++) {
-			if(clue.substring(i, i+1).equals(combo.substring(i, i+1))) {
+			if(letterList[i].equals(combo.substring(i, i+1))) {
 				newString += clue.substring(i, i+1);
 			}
 			else if(combo.contains(clue.substring(i, i+1))) {
