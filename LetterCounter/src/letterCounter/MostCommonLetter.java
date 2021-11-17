620db3d03a9b366c793f900c5ac40e3f5fb472f3
oliverstivers <oliverstivers@gmail.com>
2021-11-17 10:19:03 -0800

    exports csv

--- LetterCounter/src/letterCounter/MostCommonLetter.java
+++ LetterCounter/src/letterCounter/MostCommonLetter.java
@@ -1,10 +1,13 @@
 package letterCounter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.File;
 import java.util.*;
+import java.io.*;
 public class MostCommonLetter {
 
-	public static void main(String[] args) throws FileNotFoundException{
+	public static void main(String[] args) throws FileNotFoundException, IOException{
 		Scanner user = new Scanner(System.in);
 		System.out.println("Enter a file path: ");
 		File f = new File(user.nextLine());
@@ -14,7 +17,7 @@ public class MostCommonLetter {
 		while(in.hasNext()) {
 			String line = in.nextLine();
 			Scanner lineScanner = new Scanner(line);
-			lineScanner.useDelimiter(" ");
+			lineScanner.useDelimiter("");
 			while(lineScanner.hasNext()) {
 				String letter = lineScanner.next();
                 letter = letter.replaceAll("[^a-zA-Z]", "");
@@ -56,11 +59,15 @@ public class MostCommonLetter {
         String maxLetterChar = "";
         List<Integer> numList = new ArrayList<>();
         List<String> charList = new ArrayList<>();
+        
+        
         charList.add(minLetterChar);
         charList.add(maxLetterChar);
         numList.add(minLetter);
         numList.add(maxLetter);
+        
         sorted.forEach((key, value) -> {
+            
             if(value < numList.get(0)){
                 numList.set(0, value);
                 charList.set(0, key);
@@ -73,7 +80,7 @@ public class MostCommonLetter {
         
         System.out.println("The least common letter in the text is " + charList.get(0) + " with " + numList.get(0) + " occurrences.");
         System.out.println("The most common letter in the text is " + charList.get(1) + " with " + numList.get(1) + " occurrences.");
-        System.out.println("Would you like the see the full letter list? (y or n): ");
+        System.out.println("Would you like the see the full letter list and percentages? (y or n): ");
         String answer = user.nextLine();
         while(!(answer.equals("y") || answer.equals("n"))){
             System.out.println("Symbol " + answer + " ot recognized, please try again: ");
@@ -81,10 +88,45 @@ public class MostCommonLetter {
         }
         if(answer.toLowerCase().equals("y")){
             sorted.forEach((key, value) -> {
-                System.out.println(key + ": " + value);
+                System.out.println(key + " " + value);
             });
+            System.out.println("Percentages: ");
+            sorted.forEach((key, value) -> {
+                System.out.println(key + " " + (double)value / (double)list.size() * 100);
+            });
+            
         }
-        
+        System.out.println("Would you like to export csv? (y or n): ");
+        String csvAns = user.nextLine();
+        if(csvAns.toLowerCase().equals("y")){
+            System.out.println("Enter file path and name to write csv to: ");
+            String csvPath = user.nextLine();
+            String csvName = user.nextLine();
+            try{
+                BufferedWriter bw = new BufferedWriter(
+                new FileWriter(csvPath + "\\" + csvName + ".csv"));
+                for(Map.Entry<String, Integer> line: sorted.entrySet()){
+                    bw.write(line.getKey() + "," + line.getValue());
+                    bw.newLine();
+                }
+                BufferedWriter percent = new BufferedWriter(new FileWriter(csvPath + "\\" + csvName + "percents.csv"));
+                for(Map.Entry<String, Integer> percentLine: sorted.entrySet()){
+                    percent.write(percentLine.getKey() + "," + (double)percentLine.getValue() / list.size() * 100);
+                    percent.newLine();
+                }
+                percent.flush();
+                percent.close();
+                bw.flush();
+                bw.close();
+                
+            }
+            catch(Exception e){
+                user.close();
+                in.close();
+                return;
+            }
+            
+        }
 
     
         in.close();
