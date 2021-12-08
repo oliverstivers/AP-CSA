a4b34346f0befe8c11576871e89482ebca20ddb8
oliverstivers <oliverstivers@gmail.com>
2021-12-08 11:58:49 -0800

    Update PersonalityTest.java

--- PersonalityTest/PersonalityTest/src/PersonalityTest.java
+++ PersonalityTest/PersonalityTest/src/PersonalityTest.java
@@ -1,18 +1,34 @@
+
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.io.FileNotFoundException;
 import java.io.File;
+import java.io.PrintStream;
 public class PersonalityTest extends PersonalityTestMethods{
     public static void main(String[] args) throws FileNotFoundException {
          ArrayList<String> names = new ArrayList<>();
          ArrayList<String> answers = new ArrayList<>();
-         File f = new File("c:/users/olive/documents/github/ap-csa/personalitytest/personalitytest/testFile.txt");
+         introduce();
+         Scanner in = new Scanner(System.in);
+         System.out.print("input file name? ");
+         File f = new File(in.next());
+         System.out.print("output file name? ");
+         File output = new File(in.next());
+         PrintStream printStream = new PrintStream(output);
          nameAndAnswerArray(names, answers, f);
-         System.out.println(names);
-         System.out.println(answers);
-         ArrayList<String> personality = new ArrayList<>();
+         
+         
+         ArrayList<Integer> percentList = new ArrayList<>();
+         
          for(int i = 0; i < names.size(); i++){
-            personality.add(getPersonality(answers.get(i)));
+             String thisPersonality = getPersonality(answers.get(i), percentList);
+
+             printStream.print(names.get(i) + ": " + percentList + " = " + thisPersonality);
+             printStream.println();
+             percentList.clear();
          }
-         System.out.println(personality);
+         
+         printStream.close();
+         in.close();
     }
 }
