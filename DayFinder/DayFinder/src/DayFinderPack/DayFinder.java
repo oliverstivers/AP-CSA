93de3b6fda56026e190245dc968a047a18d64584
oliverstivers <oliverstivers@gmail.com>
2021-12-16 15:38:25 -0800

    Update DayFinder.java

--- DayFinder/DayFinder/src/DayFinderPack/DayFinder.java
+++ DayFinder/DayFinder/src/DayFinderPack/DayFinder.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 public class DayFinder extends DayFinderFormulas{
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter day number: ");
-        int day = in.nextInt();
         System.out.println("Enter month number: ");
         int month = in.nextInt();
+        System.out.println("Enter day number: ");
+        int day = in.nextInt();
         System.out.println("Enter year (4 digits):");
         int year = in.nextInt();
         getDay(day, month, year);
