029719c9faa8c302caf9a102c82a0f913252e219
oliverstivers <oliverstivers@gmail.com>
2021-12-16 10:04:36 -0800

    Update GuessingGame.java

--- GuessingGame/src/GuessingGame/GuessingGame.java
+++ GuessingGame/src/GuessingGame/GuessingGame.java
@@ -13,37 +13,27 @@ public class GuessingGame {
 	private static int guess;
 	private static Scanner guessScanner = new Scanner(System.in);
 	private static Scanner in = new Scanner(System.in);
-	
-	
-	
-	public static void main(String[] args) {
-	
-		
-		
-		String option = "Y";
+	public static void main(String[] args) 
+    {
+    	String option = "Y";
 		introduce();
 		//while user keeps answering 'yes'
-		while(option.toUpperCase().startsWith("Y")) {
+		while(option.toUpperCase().startsWith("Y"))
+         {
 			playOneGame();
 			System.out.println("Would you like to play another game? (y or n) ");
 			option = in.nextLine();
-			// do {
-				
-			// } while (!in.hasNextLine());
-			
-				
 		}
 		printResults();
         in.close();
-		
 	}
-	private static void introduce() {
+	private static void introduce() 
+    {
 		System.out.println("This program allows you to play a guessing game. \nI will think of a number between "
 				+ MIN_NUMBER + " and " + MAX_NUMBER + " and will allow you to guess until you get it. \nFor each guess, I will tell you whether the right answer is higher or lower than your guess");
 	}
-	private static void playOneGame() {
-		
-		
+	private static void playOneGame() 
+    {
 	    guesses = 0;
 	    //generates random number
 	    int number = (int)(Math.random() * MAX_NUMBER + MIN_NUMBER);
@@ -53,17 +43,21 @@ public class GuessingGame {
 	    validInput();
 	    guesses++;
 		//while user guesses wrong
-	    while(guess != number) {
-	    	if(guess > number) {
+	    while(guess != number) 
+        {
+	    	if(guess > number) 
+            {
 	    		System.out.println("It's lower.");
 	    		guesses++;
 	    	}
-	    	else if(guess < number) {
+	    	else if(guess < number) 
+            {
 	    		System.out.println("It's higher.");
 	    		guesses++;
 	    	}
 			//if guess = random number
-	    	else {
+	    	else 
+            {
 	    		guess++;
 	    		break;
 	    		
@@ -71,15 +65,18 @@ public class GuessingGame {
 	    	System.out.print("Your guess? ");
 	    	validInput();
 	    }
-	    if(guesses == 1) {
+	    if(guesses == 1) 
+        {
 	    	System.out.println("You got it right in " + guesses + " guess");
 	    }
-	    else {
+	    else 
+        {
 	    	System.out.println("You got it right in " + guesses + " guesses");
 	    }
 	    totalGuesses += guesses;
 	    totalGames++;
-	    if(guesses < bestGame) {
+	    if(guesses < bestGame) 
+        {
 	    	bestGame = guesses;
 	    }
 		
@@ -87,28 +84,22 @@ public class GuessingGame {
 
 	    
 	}
-	private static void printResults() {
+	private static void printResults() 
+    {
 		System.out.println("Overall results: ");
 		System.out.println("total games = " + totalGames);
 		System.out.println("total guesses = " + totalGuesses);
 		System.out.println("guesses/game = " + (double)((double)totalGuesses / (double)totalGames ));
 		System.out.println("Best game = " + bestGame);
 	}
-	private static void validInput(){
-        
-        while (!guessScanner.hasNextInt()) {
+	private static void validInput()
+    {     
+        while (!guessScanner.hasNextInt()) 
+        {
             System.out.println("Invalid data, try again");
             guessScanner.next();
         }
-        guess = guessScanner.nextInt();
-		
-        
-        
-        
-        
+        guess = guessScanner.nextInt();      
        
     }
-	
-	
-
 }
