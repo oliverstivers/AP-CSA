253e60ad81bf4518b9dfc95cc253d09d6043aaba
oliverstivers <oliverstivers@gmail.com>
2022-01-05 21:17:32 -0800

    create pythagoras

---
+++ Pythagoras/src/Pythagoras.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class Pythagoras {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		String choice  = "";
+		
+		
+		while(!(choice.equals("H") && choice.equals("L"))) {
+			System.out.println("Would you like to enter two legs and find the hypotenuse (l), or enter a leg and a hypotenuse and find the other leg (h)?");
+			choice = in.nextLine().toUpperCase();
+			if(choice.equals("L")) {
+				System.out.println("Enter the leg lengths");
+				double length1 = in.nextDouble();
+				double length2 = in.nextDouble();
+				
+				System.out.println("The hypotenuse is " + TriangleFinder.findHypotenuse(length1, length2));
+				break;
+			}
+			else if(choice.equals("H")) {
+				double length1 = 0;
+				double length2 = 0;
+				
+				
+				
+				while(length1 == length2) {
+					System.out.println("Enter the leg and hypotenuse");
+					length1 = in.nextDouble();
+					length2 = in.nextDouble();
+					if(length1 > length2) {
+						
+						System.out.println("The missing leg is " + TriangleFinder.findLeg(length2, length1));
+						break;
+					}
+					else if(length2 > length1) {
+						
+						System.out.println("The missing leg is " + TriangleFinder.findLeg(length1, length2));
+						break;
+					}
+					System.out.println("Hypotenuse and leg cannot be equal, please try again:");
+				}
+				break;
+			}
+			else {
+				System.out.println("Symbol " + choice + " not recognized, please try again: ");
+			}
+			
+			
+		}
+		
+		in.close();
+
+	}
+
+}
---
+++ Pythagoras/src/TriangleFinder.java
@@ -0,0 +1,16 @@
+
+public class TriangleFinder {
+	
+	
+	
+	
+	public static double findHypotenuse(double side1, double side2) {
+		double hypotenuse = Math.sqrt((Math.pow(side1, 2) + Math.pow(side2, 2)));
+		return hypotenuse;
+	}
+	public static double findLeg(double side1, double side2) {
+		double leg = Math.sqrt((Math.pow(side2, 2) - Math.pow(side1, 2)));
+		return leg;
+	}
+	
+}
