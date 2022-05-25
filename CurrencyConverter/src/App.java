c8f01660d6eb4227ae17ab8dea8cd021a56cdab7
oliverstivers <oliverstivers@gmail.com>
2022-05-24 19:11:47 -0700

    remove unnecessary code

--- CurrencyConverter/src/App.java
+++ CurrencyConverter/src/App.java
@@ -1,20 +1,9 @@
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.*;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import org.json.simple.JSONArray;
-import org.json.simple.parser.JSONParser;
 
 public class App {
     public static void main(String[] args) throws Exception {
         new MyFrame();
-        // Setting URL
         
-        // Accessing object
         
         
         
